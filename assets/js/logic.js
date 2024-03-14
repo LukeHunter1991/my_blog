43cbc55a8d6e41e355dcf03d85dc486842b6fc2c
Luke Hunter <luke.hunter9@gmail.com>
2024-03-14 20:47:55 +1100

    Added notes to logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,14 +1,19 @@
+// Creates variables for the light/dark mode toggle and the body element.
 const switchEl = document.querySelector("#display-toggle");
 const bodyEl = document.querySelector("body")
-const backBtn = document.querySelector("#back")
 
+// Event listener for the light/dark mode toggle.
 switchEl.addEventListener('click', function () {
+    // Gets curent display mode of the switch element.
     state = switchEl.dataset.status;
 
+    // If light when the element is clicked, will change to dark mode by updating the class in the body element.
     if (state === 'light') {
         switchEl.dataset.status = 'dark';
         switchEl.textContent = '🌙';
         bodyEl.setAttribute('class', 'dark');
+
+    // If dark when the element is clicked, will change to light mode by updating the class in the body element.
     } else {
         switchEl.dataset.status = 'light';
         switchEl.textContent = '🌞';
